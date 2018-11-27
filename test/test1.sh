eec1d4f21e23a5a9e6536df77a5e44db980a74e4
Marc GC <carnicer@hp.com>
2018-11-27 11:46:58 +0100

    exit if error on any test

--- test/test1.sh
+++ test/test1.sh
@@ -21,21 +21,21 @@ REF_F_PT_WSOS=ref_${OUTF_PT_WSOS}
 echo "generating -r 4 -b IGNORE -s REGULAR =>" $OUTF_REGULAR
 python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REGULAR -s REGULAR >/dev/null
 echo "comparing reference" ${REF_F_REGULAR} "with generated" ${OUTF_REGULAR}
-diff -q ${REF_F_REGULAR} ${OUTF_REGULAR} || { echo "test FAILED" ; echo 1 ; }
+diff -q ${REF_F_REGULAR} ${OUTF_REGULAR} || { echo "test FAILED" ; exit 1 ; }
 echo "[test OK]"
 rm ${OUTF_REGULAR}
 
 echo "generating -r 4 -b IGNORE -s REGULARSOS =>" $OUTF_REG_SOS
 python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REG_SOS -s REGULARSOS >/dev/null
 echo "comparing reference" ${REF_F_REG_SOS} "with generated" ${OUTF_REG_SOS}
-diff -q ${REF_F_REG_SOS} ${OUTF_REG_SOS} || { echo "test FAILED" ; echo 1 ; }
+diff -q ${REF_F_REG_SOS} ${OUTF_REG_SOS} || { echo "test FAILED" ; exit 1 ; }
 echo "[test OK]"
 rm ${OUTF_REG_SOS}
 
 echo "generating -r 4 -b IGNORE -s WSOS =>" $OUTF_PT_WSOS
 python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_PT_WSOS -s WSOS >/dev/null
 echo "comparing reference" ${REF_F_PT_WSOS} "with generated" ${OUTF_PT_WSOS}
-diff -q ${REF_F_PT_WSOS} ${OUTF_PT_WSOS} || { echo "test FAILED" ; echo 1 ; }
+diff -q ${REF_F_PT_WSOS} ${OUTF_PT_WSOS} || { echo "test FAILED" ; exit 1 ; }
 echo "[test OK]"
 rm ${OUTF_PT_WSOS}
 
