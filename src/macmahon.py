49b8ca0be9c2e6b19102e475d167454196ea634c
Marc GC <carnicer@hp.com>
2018-11-27 12:31:23 +0100

    remove TODOs

--- src/macmahon.py
+++ src/macmahon.py
@@ -34,11 +34,8 @@ import getopt
 
 # TODO : move options treatment to Settings class?
 
-# TODO : fix doc -b => -s, sort => sorts, 'This may be' => 'The script may be', 'is not played' => 'is not playing'
-# TODO : autoname should consider '\' as folders (os.name == 'nt' / 'posix')
 # TODO : implement weighted SODOS (draw*1, win*3)
 # TODO : consider setting the value of a win and a loss (0, 1, 2) instead of (0, 1, 3)
-# TODO : change team names in doc so that no junior/polo/athc appear
 # TODO : display pairings (as with gerlach's sw)
 # TODO : make the team declaration section optional, but reject teams after 1st round (BYE is mandatory if odd)
 # TODO : allow long team names, with more than 1 word
