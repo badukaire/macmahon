2ce2f61ec126fc4c9586854a468588476d46f918
Marc GC <carnicer@hp.com>
2018-11-27 11:21:47 +0100

    Revert "test file names (remove)"
    This reverts commit f429c5ee57fee2b354c5c5178d7bc469b4c12bcb.

--- test/test1.sh
+++ test/test1.sh
@@ -18,28 +18,12 @@ REF_F_REGULAR=ref_${OUTF_REGULAR}
 REF_F_REG_SOS=ref_${OUTF_REG_SOS}
 REF_F_PT_WSOS=ref_${OUTF_PT_WSOS}
 
-echo OUTF_REGULAR $OUTF_REGULAR
-echo OUTF_REG_SOS $OUTF_REG_SOS
-echo OUTF_PT_WSOS $OUTF_PT_WSOS
-
-echo "REF with -O : test1_sortRegular_byeIgnore_r4_4.txt"
-echo "cmp with -o : $OUTF_REGULAR"
-
-echo REF_F_REGULAR $REF_F_REGULAR
-echo REF_F_REG_SOS $REF_F_REG_SOS
-echo REF_F_PT_WSOS $REF_F_PT_WSOS
-
-echo "REF with -O : test1_sortRegular_byeIgnore_r4_4.txt"
-echo "cmp test: $REF_F_REGULAR"
-
-
 echo "generating -r 4 -b IGNORE -s REGULAR =>" $OUTF_REGULAR
 python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REGULAR -s REGULAR >/dev/null
 echo "comparing reference" ${REF_F_REGULAR} "with generated" ${OUTF_REGULAR}
 diff -q ${REF_F_REGULAR} ${OUTF_REGULAR} || { echo "test FAILED" ; echo 1 ; }
 echo "[test OK]"
 rm ${OUTF_REGULAR}
-
 
 echo "generating -r 4 -b IGNORE -s REGULARSOS =>" $OUTF_REG_SOS
 python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REG_SOS -s REGULARSOS >/dev/null
