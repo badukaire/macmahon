ade87411edadc9ba5eb885365e28e500ff8fb85a
Marc GC <carnicer@hp.com>
2018-11-27 11:04:49 +0100

    fix script name, throw away output && rm output

--- test/test1.sh
+++ test/test1.sh
@@ -3,8 +3,9 @@
 INPUT=test1
 F_INPUT=${INPUT}.txt
 DIR_IN=../testData
-
 T01F=$DIR_IN/$F_INPUT
+
+PYSCRIPT=../src/macmahon.py
 
 BYE="byeIgnore"
 RNDS="r4_4"
@@ -18,17 +19,23 @@ REF_F_REG_SOS=ref_${OUTF_REG_SOS}
 REF_F_PT_WSOS=ref_${OUTF_PT_WSOS}
 
 echo "generating -r 4 -b IGNORE -s REGULAR =>" $OUTF_REGULAR
-python p.py -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REGULAR -s REGULAR
+python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REGULAR -s REGULAR >/dev/null
 echo "comparing reference" ${REF_F_REGULAR} "with generated" ${OUTF_REGULAR}
 diff -q ${REF_F_REGULAR} ${OUTF_REGULAR} || { echo "test FAILED" ; echo 1 ; }
+echo "[test OK]"
+rm ${OUTF_REGULAR}
 
 echo "generating -r 4 -b IGNORE -s REGULARSOS =>" $OUTF_REG_SOS
-python p.py -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REG_SOS -s REGULARSOS
+python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_REG_SOS -s REGULARSOS >/dev/null
 echo "comparing reference" ${REF_F_REG_SOS} "with generated" ${OUTF_REG_SOS}
 diff -q ${REF_F_REG_SOS} ${OUTF_REG_SOS} || { echo "test FAILED" ; echo 1 ; }
+echo "[test OK]"
+rm ${OUTF_REG_SOS}
 
 echo "generating -r 4 -b IGNORE -s WSOS =>" $OUTF_PT_WSOS
-python p.py -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_PT_WSOS -s WSOS
+python $PYSCRIPT -f $T01F -r 4 -d TABLE -b IGNORE -o $OUTF_PT_WSOS -s WSOS >/dev/null
 echo "comparing reference" ${REF_F_PT_WSOS} "with generated" ${OUTF_PT_WSOS}
 diff -q ${REF_F_PT_WSOS} ${OUTF_PT_WSOS} || { echo "test FAILED" ; echo 1 ; }
+echo "[test OK]"
+rm ${OUTF_PT_WSOS}
 
